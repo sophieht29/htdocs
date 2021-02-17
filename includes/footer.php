88f3d8c2d4e91e50f129cb66c2b403e283af0148
sophietorp <soph1374@edu.easj.dk>
2021-02-17 15:46:34 +0100

    git init

---
+++ includes/footer.php
@@ -0,0 +1,17 @@
+<div class="footer">
+    <div>
+        <a href="#">link</a>
+        <a href="#">link</a>
+        <a href="#">link</a>
+    </div>
+    <div>
+        <a href="#">link</a>
+        <a href="#">link</a>
+        <a href="#">link</a>
+    </div>
+    <div>
+        <a href="#">link</a>
+        <a href="#">link</a>
+        <a href="#">link</a>
+    </div>
+</div>
---
+++ includes/navigation.php
@@ -0,0 +1,8 @@
+<div class="navigation">
+    <a href="#">forside</a>
+    <a href="#">om os</a>
+    <a href="contact.php">kontakt</a>
+    <a href="#">ny side</a>
+</div>
+
+
